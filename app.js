744382efbdc79932ce9e82d14d7f9a745c1939ee
alfirosyadi <muhammadalfirosyadi1998@gmail.com>
2023-01-29 19:28:05 +0700

    Express JS Middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('In the middleware here');
+    next(); //Allows the request to continue to the next middleware in Line
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware here');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
