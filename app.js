6e120322bd4b4791b07609d52ae2ef76e2286c34
alfirosyadi <muhammadalfirosyadi1998@gmail.com>
2023-01-29 19:43:19 +0700

    Handling different request

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware here');
-    next(); //Allows the request to continue to the next middleware in Line
-});
-
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In another middleware here');
+    res.send('<h>Add Product Page</h>');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log('In another middleware here');
+    res.send('<h>hello from express</h>');
+});
 
-server.listen(3000);
+app.listen(3000);
