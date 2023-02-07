bfdcdb813e4687bfa797cccc1136a317f41779b9
alfirosyadi <muhammadalfirosyadi1998@gmail.com>
2023-02-07 19:36:17 +0700

    styling pages

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/add-product', (req, res, next) => {
     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
-router.post('/product', (req, res, next) => {
+router.post('/', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
